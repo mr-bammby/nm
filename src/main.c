8246bf3e77b4d4e7ba10390c803b5c366de35a4b
Domen Banfi <dbanfi@student.42wolfsburg.de>
2024-11-05 16:00:10 +0100

    Implemented symbol parsing.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "../ElfParser/inc_pub/elfparser_header.h"
 #include "../ElfParser/inc_pub/elfparser_secthead.h"
-#include "../ElfParser/inc_pub/elfparser_strtable.h"
+#include "../ElfParser/inc_pub/elfparser_symtable.h"
 #include "../FileHandler/inc_pub/filehandler.h"
 #include <stdio.h>
 
@@ -9,6 +9,7 @@ int main (int argc, char **argv)
     source_file_t file = {0};
     elfparser_header_t elf_header = {0};
     elfparser_secthead_t elf_sect_head = {0};
+    elfparser_symtable_t elf_symbol_table = {0};
 
     int ret;
 
@@ -82,12 +83,52 @@ int main (int argc, char **argv)
         printf("Problem section header name resolving, With err: %d\n", ret);
     }
     printf("---------------------------!--------\n");
-    printf("idx|size|off|name\n");
+    printf("idx|size|off|name|entry_size\n");
     for (int i = 0; i < elf_sect_head.table_len; i++)
     {
-        printf("%d|%lX|%lX|%s\n", i, (elf_sect_head.table)[i].sh_size, (elf_sect_head.table)[i].sh_offset, (elf_sect_head.table)[i].sh_name);
+        printf("%d|%lX|%lX|%s|%lX\n", i, (elf_sect_head.table)[i].sh_size, (elf_sect_head.table)[i].sh_offset, (elf_sect_head.table)[i].sh_name, (elf_sect_head.table)[i].sh_entsize);
     }
-
+    int32_t symtab_sect_index = ElfParser_SectHead_byNameFind(&elf_sect_head, ".symtab", 0);
+    if  (symtab_sect_index < 0)
+    {
+        printf("Problem with reding section index, Withh err: %d\n", symtab_sect_index);
+        return(1);
+    }
+    printf("Index: %d\n", symtab_sect_index);
+    printf("off: %lx, size: %lx\n", (elf_sect_head.table)[symtab_sect_index].sh_offset, (elf_sect_head.table)[symtab_sect_index].sh_size);
+    ret = FileHandler_mapGet(&file, elf_sect_head.table[symtab_sect_index].sh_size, elf_sect_head.table[symtab_sect_index].sh_offset);
+    if (ret)
+    {
+        printf("Problem with getting a map, Withh err: %d\n", ret);
+    }
+    ret = ElfParser_SymTable_structSetup(&elf_symbol_table, &elf_sect_head, symtab_sect_index, &elf_header);
+    if (ret)
+    {
+        printf("Problem struct setup, With err: %d\n", ret);
+    }
+    ret = ElfParser_SymTable_parse(&elf_symbol_table, file.map, file.map_len);
+    if (ret)
+    {
+        printf("Problem ssymbol table parsing, With err: %d\n", ret);
+    }
+    printf("off: %lx, size: %lx\n", (elf_sect_head.table)[elf_symbol_table.string_table_idx].sh_offset, (elf_sect_head.table)[elf_symbol_table.string_table_idx].sh_size);
+    ret = FileHandler_mapGet(&file, (elf_sect_head.table)[elf_symbol_table.string_table_idx].sh_size, (elf_sect_head.table)[elf_symbol_table.string_table_idx].sh_offset);
+    if (ret)
+    {
+        printf("Problem with getting a map, Withh err: %d\n", ret);
+    }
+    ret = ElfParser_SymTable_nameResolve(&elf_symbol_table, file.map,  file.map_len);
+    if (ret)
+    {
+        printf("Problem section header name resolving, With err: %d\n", ret);
+    }
+    printf("---------------------------!--------\n");
+    printf("idx|name|value|binding|section_idx\n");
+    for (int i = 0; i < elf_symbol_table.table_len; i++)
+    {
+        printf("%d|%s|%lX|%x|%d\n", i, (elf_symbol_table.table)[i].sym_name, (elf_symbol_table.table)[i].sym_value, (elf_symbol_table.table)[i].sym_bind, (elf_symbol_table.table)[i].sym_sect_idx);
+    }
+    ElfParser_SymTable_free(&elf_symbol_table);
     ElfParser_SectHead_free(&elf_sect_head);
     FileHandler_fileClose(&file);
 }
